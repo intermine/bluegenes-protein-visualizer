7e26008fd46a45d6e6e0224c7ffd047d575e5cde
akshat <akshatbhargavadeepa@gmail.com>
2019-06-08 09:06:35 +0530

    added geneToProtein query

--- src/RootContainer.js
+++ src/RootContainer.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import queryGeneToProtein from './queries/geneToProtein';
 
 class RootContainer extends React.Component {
+	componentDidMount() {
+		const {
+			entity: { value: geneId },
+			serviceUrl
+		} = this.props;
+
+		// get all proteins with their accession ids associated with the gene
+		queryGeneToProtein(geneId, serviceUrl).then(() => {
+			// const { proteins } = res;
+			// console.log(proteins.map(p => p.primaryAccession));
+		});
+
+		// get pdb file for each protein
+	}
+
 	render() {
 		return (
 			<div className="rootContainer">
